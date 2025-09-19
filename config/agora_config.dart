dfd4743ee20631e6b9371e9bc68b10cfc3eeea8a
labibanis786 <147445808+labibanis786@users.noreply.github.com>
2025-09-20 02:53:57 +0530

    add lib/config/agora_config.dart with App ID

---
+++ config/agora_config.dart
@@ -0,0 +1,10 @@
+class AgoraConfig {
+  // Your Agora App ID (from Agora Console)
+  static const String appId = "739a61014ae64d2eaf7dd9eb4872664e";
+
+  // Token (leave empty if not required)
+  static const String token = "";
+
+  // Default channel name
+  static const String channelName = "bigfun_channel";
+}
